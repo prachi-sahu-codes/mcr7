fd45b14642200d5ce056a713b9cdceae454e938b
SahuPrachi <prachisahu998@gmail.com>
2023-07-09 22:11:46 +0530

    removing unused vars

--- src/pages/DestinationDetail.js
+++ src/pages/DestinationDetail.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { useParams } from "react-router";
-import { useNavigate } from "react-router";
-import { Card } from "../component/Card";
 import { useData } from "../context/DataContext";
 
 export const DestinationDetail = () => {
-  const navigate = useNavigate();
   const { detail } = useParams();
   const { destination } = useData();
 
